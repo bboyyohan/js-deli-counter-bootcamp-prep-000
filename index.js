f84d0008c2f96c8f4c0d662a84714b0b8c95574c
stupendous-patch-9345 <yo.park93@yahoo.com>
2019-10-01 18:53:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var katzDeliLine = [];
 
 function takeANumber(katzDeliLine, name) {
-  katzDeliLine.push(`Welcome, ${name}. You are number ${katzDeliLine.length} in line.`)
+  katzDeliLine.push(name);
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
