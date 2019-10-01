14509d75c4b648ba6a641dc33bd957eae1c91ef5
stupendous-patch-9345 <yo.park93@yahoo.com>
2019-10-01 18:57:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,12 @@ function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name);
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length > 0) {
+  katzDeliLine.shift()
+  var nowServing = katzDeliLine[0];
+  return `Currently serving
+  }
+  
+}
