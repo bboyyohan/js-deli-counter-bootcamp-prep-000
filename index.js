dbeaf3eb5b25b1a6e84ed7664670c87d40eccc5e
stupendous-patch-9345 <yo.park93@yahoo.com>
2019-10-01 19:03:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ function nowServing(katzDeliLine) {
   if (katzDeliLine.length > 0) {
   katzDeliLine.shift()
   var nowServing = katzDeliLine[0];
-  return `Currently serving ${nowServing}
+  return `Currently serving ${nowServing}.`
+  } else {
+    return `The line is currently empty.`
   }
-  
 }
