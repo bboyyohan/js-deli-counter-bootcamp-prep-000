047cd6f0c90b7ab1d43e160fb25af32ed43e490a
stupendous-patch-9345 <yo.park93@yahoo.com>
2019-10-01 18:50:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,30 +1,28 @@
 var katzDeliLine = [];
 
 function takeANumber(katzDeliLine, name) {
-  katzDeliLine.push(name)
-  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
+  katzDeliLine.push(name);
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
 
 function nowServing(katzDeliLine) {
   if (katzDeliLine.length > 0) {
-    var nowServing = katzDeliLine[0];
+    var nowServing = katzDeliLine[0]
     katzDeliLine.shift();
-    return `Currently serving ${nowServing}.`;
-    
+    return `Currently serving ${nowServing}.`
   } else {
-    return `There is nobody waiting to be served!`;
+    return `There is nobody waiting to be served!`
   }
 }
-
-function currentLine(line) {
-  if (line.length === 0) {
-    return `The line is currently empty.`;
-  } else {
-
-    var array = [];
-    for (var i = 0; i < line.length; i++) {
-      array.push(` ${i+1}. ${line[i]}`);
-    }
-    return `The line is currently:` + array;
+var array = [];
+function currentLine(katzDeliLine) {
+  let i = 0;
+  while (i < katzDeliLine.length) {
+    katzDeliLine.push(` ${i+1}. ${katzDeliLine.length[i]}`)
+    i++;
   }
+  if (katzDeliLine === 0) {
+    return `The line is currently empty.`
+  } else 
+    return `The line currently:` + array;
 }
